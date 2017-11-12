80594ebb7965e236ae07fa127bfc63f25fb3c4c9
Friedrich von Never <friedrich@fornever.me>
2017-11-12 17:19:06 +0700

    #2: add BinaryXamlWriter stub, enhance tests

--- XamlCompiler.Tests/XamlCompilerTest.cs
+++ XamlCompiler.Tests/XamlCompilerTest.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using Portable.Xaml;
 using XamlCompiler.Tests.TestClasses;
 using Xunit;
@@ -8,8 +10,50 @@ namespace XamlCompiler.Tests
 {
     public class XamlCompilerTest
     {
+        private static T ActivateInstanceFromXaml<T>(string xaml, string typeName)
+        {
+            var context = new XamlSchemaContext(new[] {typeof(Window).Assembly});
+            using (var xamlReader = new StringReader(xaml))
+            using (var reader = new XamlXmlReader(xamlReader, context))
+            using (var writer = new BinaryXamlWriter(context, "test", new Version()))
+            using (var stream = new MemoryStream())
+            {
+                XamlServices.Transform(reader, writer);
+                var bytes = stream.ToArray();
+                var assembly = Assembly.Load(bytes);
+                var type = assembly.ExportedTypes.Single(t => t.Name == "TestClass");
+                return (T)Activator.CreateInstance(type);
+            }
+        }
+
         [Fact]
-        public void ObjectWriterGeneratesInstanceOfRightType()
+        public void SimpleClassIsDefined()
+        {
+            const string xaml = @"<Window xmlns=""test.fornever.me""
+        xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+        x:Class=""TestClass"" />";
+
+            var window = ActivateInstanceFromXaml<Window>(xaml, "TestClass");
+            Assert.Equal("TestClass", window.GetType().Name);
+        }
+
+        [Fact]
+        public void PropertiesAreSetFromXaml()
+        {
+            const string xaml = @"<Window xmlns=""test.fornever.me""
+        xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
+        x:Class=""TestClass""
+        StringProperty=""TestString""
+        IntProperty=""42"">
+</Window>";
+
+            var window = ActivateInstanceFromXaml<Window>(xaml, "TestClass");
+            Assert.Equal("TestString", window.StringProperty);
+            Assert.Equal(42, window.IntProperty);
+        }
+
+        [Fact]
+        public void ChildIsCreatedFromXaml()
         {
             const string xaml = @"<Window xmlns=""test.fornever.me""
         xmlns:x=""http://schemas.microsoft.com/winfx/2006/xaml""
@@ -19,14 +63,9 @@ namespace XamlCompiler.Tests
     <Control x:Name=""ChildControl"" />
 </Window>";
 
-            var context = new XamlSchemaContext(new[] {typeof(Window).Assembly});
-            using (var xamlReader = new StringReader(xaml))
-            using (var reader = new XamlXmlReader(xamlReader, context))
-            using (var writer = new XamlObjectWriter(context))
-            {
-                XamlServices.Transform(reader, writer);
-                Assert.Equal(typeof(Window), writer.Result.GetType());
-            }
+            var window = ActivateInstanceFromXaml<Window>(xaml, "TestClass");
+            var child = window.Child;
+            Assert.NotNull(child);
         }
     }
 }
---
+++ XamlCompiler/BinaryXamlWriter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Mono.Cecil;
+using Portable.Xaml;
+
+namespace XamlCompiler
+{
+    public class BinaryXamlWriter : XamlWriter
+    {
+        private readonly ModuleDefinition _module;
+
+        private XamlType _baseType;
+        private TypeDefinition _currentType;
+
+        public BinaryXamlWriter(XamlSchemaContext context, string name, Version version)
+        {
+            SchemaContext = context;
+
+            var assemblyName = new AssemblyNameDefinition(name, version);
+            _module = CreateModule(assemblyName);
+        }
+
+        public void WriteAssembly(Stream output)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static ModuleDefinition CreateModule(AssemblyNameDefinition assemblyName)
+        {
+            var assembly = AssemblyDefinition.CreateAssembly(
+                assemblyName,
+                assemblyName.Name,
+                ModuleKind.Dll);
+            return assembly.MainModule;
+        }
+
+        public override XamlSchemaContext SchemaContext { get; }
+
+        public override void WriteEndMember()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteEndObject()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteGetObject()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteNamespace(NamespaceDeclaration namespaceDeclaration)
+        {
+        }
+
+        public override void WriteStartMember(XamlMember xamlMember)
+        {
+            switch (xamlMember)
+            {
+                case var x when x == XamlLanguage.Class:
+                    Debug.Assert(_currentType == null);
+                    _currentType = new TypeDefinition("", "", TypeAttributes.Class);
+                    break;
+                default:
+                    throw new NotSupportedException($"XAML member {xamlMember} is not supported by the compiler");
+            }
+        }
+
+        public override void WriteStartObject(XamlType type)
+        {
+            Debug.Assert(_baseType == null);
+            _baseType = type;
+        }
+
+        public override void WriteValue(object value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
